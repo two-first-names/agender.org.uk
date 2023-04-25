cc20f776f47e3489432d1da882e43fe0f1c91c26
Joe Roberts <joe@twofirstnames.org>
2023-04-25 16:51:49 +0100

    Add Deploy workflow

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   return {
     dataTemplateEngine: 'njk',
